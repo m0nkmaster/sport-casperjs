7f34e1a126b154b7ccae6c5e2cf34c8f37f123ba
Rob MacDonald <rob.macdonald@bbc.co.uk>
2014-11-29 22:15:16 +0000

    Fixed text fail

--- mobile.js
+++ mobile.js
@@ -11,7 +11,7 @@ casper.test.begin('Mobile Homepage Checks', function suite(test) {
 
         test.assertElementCount('article.top-lead', 1);
 
-        test.assertTextExists('Fixtlures, Scores & Results');
+        test.assertTextExists('Fixtures, Scores & Results');
 
         this.capture('homepage.png');
     });
